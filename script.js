93ecda2f2eb5c83241cd996a22f7fc727f8726c8
Vlad Vladimirov <blad20002000@mail.ru>
2022-01-28 15:12:58 +0700

    Усложненное задание второго урока

---
+++ script.js
@@ -0,0 +1,12 @@
+// Переменные
+let num = 266219
+let str = num.toString()
+let factorial = 1
+let str2 = ""
+for (let i = 0; i < str.length; i++)
+{
+    factorial *= Number(str[i]);
+}
+console.log(factorial)
+str2 = (factorial ** 3).toString();
+console.log(str2.substring(0, 2))
