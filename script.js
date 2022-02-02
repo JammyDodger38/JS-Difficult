9e4d167ae890b97ac2b9c13e2197cecd10ba5896
Vlad Vladimirov <blad20002000@mail.ru>
2022-02-02 17:35:12 +0700

    Сделал второе усложненное задание 5 урока

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 'use strict'
 
-let str = prompt("Введите строку:");
+//Первое подзадание
 
-const method = function (arg) {
-    if (arg == (arg*1)) {
-        alert("Введеные данные не являются строкой");
-    } else if (typeof arg === 'string') {
-        arg = arg.trim();
-        if (arg.length > 30) {
-            arg = arg.substring(0, 30) + "...";
-        }
-        console.log(arg);
+let arr = ["234", "777", "562", "432", "444", "987", "789"]
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] == "2" || arr[i][0] == 4) {
+        console.log(arr[i]);
     }
 }
 
-method(str);
+//Второе подзадание
+
+let esNum
+
+for (let i = 2; i < 100; i++) {
+    esNum = true;
+    for (let j = 2; j < 10; j++) {
+        if(j != i) {
+            if (i % j == 0) {
+                esNum = false
+                break
+            }
+        }
+        else {continue}
+    }
+    if (esNum == true) {
+        console.log("Делители этого числа: 1 и " + i);
+    }
+}
