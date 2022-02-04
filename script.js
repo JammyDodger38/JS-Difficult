b1f4f8fed2a0d66d21e2f78c9bb5da381cace014
Vlad Vladimirov <blad20002000@mail.ru>
2022-02-04 17:34:08 +0700

    Сделал задание по выводу на экран дней недели

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 'use strict'
 
-//Первое подзадание
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+let allDay = ""
+let d = new Date()
 
-let arr = ["234", "777", "562", "432", "444", "987", "789"]
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] == "2" || arr[i][0] == 4) {
-        console.log(arr[i]);
+for (let key in week) {
+    let temp = ""
+    temp = week[key]
+    if (key == 5 || key == 6) {
+        temp = "<i>" + temp + "</i>"
     }
+    if (key == d.getDay()-1 || key-6 == d.getDay()) {
+        temp = "<b>" + temp + "</b>"
+    }
+    allDay += temp + "<br>"
 }
 
-//Второе подзадание
-
-let esNum
-
-for (let i = 2; i < 100; i++) {
-    esNum = true;
-    for (let j = 2; j < 10; j++) {
-        if(j != i) {
-            if (i % j == 0) {
-                esNum = false
-                break
-            }
-        }
-        else {continue}
-    }
-    if (esNum == true) {
-        console.log("Делители этого числа: 1 и " + i);
-    }
-}
+document.getElementById('days').innerHTML= allDay
