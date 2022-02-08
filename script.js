3f6e72cc3e43c08710ea43f2506522b8764099fb
Vlad Vladimirov <blad20002000@mail.ru>
2022-02-08 18:25:36 +0700

    Сделал усложненное задание 9 урока

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
 'use strict'
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-let allDay = ""
-let d = new Date()
+let time1, time2 = ""
+let days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+let month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
 
-for (let key in week) {
-    let temp = ""
-    temp = week[key]
-    if (key == 5 || key == 6) {
-        temp = "<i>" + temp + "</i>"
+
+const ifOneSign = function(num) {
+    if(num .toString().length == 1) {
+        num = "0" + num
     }
-    if (key == d.getDay()-1 || key-6 == d.getDay()) {
-        temp = "<b>" + temp + "</b>"
-    }
-    allDay += temp + "<br>"
+    return num
 }
 
-document.getElementById('days').innerHTML= allDay
+const createDate = function() {
+    let d = new Date();
+    let find = ""
+
+    time1 = "Сегодня " + days[d.getDay()] + ", "
+    time1 += d.getDate() + " " + month[d.getMonth()] + " "
+    time1 += d.getFullYear() + " года, "
+
+    find = d.getHours() .toString();
+    if(find.endsWith(1)) {
+        time1 += d.getHours() + " час "
+    } else if (d.getHours() >= 17) {
+        time1 += d.getHours() + " часов "
+    } else {
+        time1 += d.getHours() + " часа "
+    }
+
+    find = d.getMinutes() .toString()
+    if (find.endsWith(1)) {
+        time1 += d.getMinutes() + " минута "
+    } else if (find.endsWith(2) || find.endsWith(3) || find.endsWith(4)) {
+        time1 += d.getMinutes() + " минуты "
+    } else {
+        time1 += d.getMinutes() + " минут "
+    }
+    
+    find = d.getSeconds() .toString()
+    if (find.endsWith(1)) {
+        time1 += d.getSeconds() + " секунда "
+    } else if (find.endsWith(2) || find.endsWith(3) || find.endsWith(4)) {
+        time1 += d.getSeconds() + " секунды "
+    } else {
+        time1 += d.getSeconds() + " секунд "
+    }
+
+    time1 = "<b>" + time1 + "</b>"
+
+    time2 = ifOneSign(d.getDate()) + "."
+    time2 += ifOneSign(d.getMonth()+1) + "."
+    time2 += d.getFullYear() + " - "
+    time2 += ifOneSign(d.getHours()) + ":"
+    time2 += ifOneSign(d.getMinutes()) + ":"
+    time2 += ifOneSign(d.getSeconds())
+
+    time2 = "<b>" + time2 + "</b>"
+
+    document.getElementById('Time1').innerHTML = time1
+    document.getElementById('Time2').innerHTML = time2
+}
+setInterval(createDate, 1000)
+
+
+
